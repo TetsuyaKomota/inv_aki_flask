7f1c5c12349bb903e69174bd3a508731d169861c
komot <komotaymkty@gmail.com>
2023-07-12 23:19:18 +0900

    [update - main.py] 適当にメイン画面を編集

--- inv_aki_flask/app.py
+++ inv_aki_flask/app.py
@@ -5,6 +5,7 @@ from inv_aki_flask.views import index, main
 app = Flask(__name__)
 app.register_blueprint(index.view)
 app.register_blueprint(main.view)
+app.secret_key = b"random string..."
 
 
 def launch_for_local():
--- inv_aki_flask/views/main.py
+++ inv_aki_flask/views/main.py
@@ -1,10 +1,75 @@
-from datetime import datetime
-
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, session
 
 view = Blueprint("main", __name__, url_prefix="/main")
+
+member_data = {}
+
+message_data = []
 
 
 @view.route("/", methods=["GET"])
 def show():
-    return render_template("main.html")
+    global message_data
+    if "login" in session and session["login"]:
+        msg = f"Login ID: {session['id']}"
+        return render_template(
+            "main.html",
+            title="逆アキネイター(仮)",
+            message=msg,
+            data=message_data,
+        )
+    else:
+        return redirect("/main/login")
+
+
+@view.route("/", methods=["POST"])
+def form():
+    global message_data
+    msg = request.form.get("comment")
+    message_data.append((session["id"], msg))
+    message_data = message_data[-25:]
+    return redirect("/main/")
+
+
+@view.route("/login", methods=["GET"])
+def login():
+    return render_template(
+        "login.html",
+        title="Login",
+        err=False,
+        message="IDとパスワードを入力: ",
+        id="",
+    )
+
+
+@view.route("/login", methods=["POST"])
+def login_post():
+    global member_data
+    id = request.form.get("id")
+    pswd = request.form.get("pass")
+
+    if id in member_data:
+        session["login"] = pswd == member_data[id]
+    else:
+        member_data[id] = pswd
+        session["login"] = True
+
+    session["id"] = id
+
+    if session["login"]:
+        return redirect("/main/")
+    else:
+        return render_template(
+            "login.html",
+            title="Login",
+            err=False,
+            message="パスワードが違います",
+            id=id,
+        )
+
+
+@view.route("/logout", methods=["GET"])
+def logout():
+    session.pop("id")
+    session.pop("login")
+    return redirect("/main/login")
