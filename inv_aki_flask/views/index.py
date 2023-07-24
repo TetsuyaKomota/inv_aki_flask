d9279eac70b7998c0de247fc9cb79e1f77dbd018
komot <komotaymkty@gmail.com>
2023-07-24 22:28:59 +0900

    [tmp - index.py] 仮のsecret を読んでみる

--- inv_aki_flask/views/index.py
+++ inv_aki_flask/views/index.py
@@ -1,11 +1,22 @@
 from datetime import datetime
 
+from google.cloud import secretmanager
+
 from flask import Blueprint, render_template
 
 view = Blueprint("index", __name__, url_prefix="/")
 
+# FIXME SecretManager の検証
+project_id = "inv_aki"
+secret_id = "hello"
+version_id = "latest"
+client = secretmanager.SecretManagerServiceClient()
+name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+response = client.access_secret_version(request={"name": name})
+payload = response.payload.data.decode("utf-8")
 
 @view.route("/", methods=["GET"])
 def show():
     yyyymmdd = datetime.now().strftime("%Y%m%d")
-    return render_template("index.html", name=yyyymmdd)
+    return render_template("index.html", name=yyyymmdd, payload=payload)
