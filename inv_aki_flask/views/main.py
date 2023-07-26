1145b1a57f31ed6f550b05952431781d80d75eaf
komot <komotaymkty@gmail.com>
2023-07-27 00:19:44 +0900

    [update - index.py] message_data を session 管理に変更

--- inv_aki_flask/views/main.py
+++ inv_aki_flask/views/main.py
@@ -6,10 +6,6 @@ from inv_aki_flask.model.chatgpt import ChatGPT
 
 view = Blueprint("main", __name__, url_prefix="/main")
 
-member_data = {}
-
-message_data = []
-
 if os.path.exists("tmp/api_key.txt"):
     with open("tmp/api_key.txt", "r") as f:
         api_key = f.read().strip()
@@ -20,22 +16,25 @@ else:
 
 @view.route("/", methods=["GET"])
 def show():
-    global message_data
-    if "login" in session and session["login"]:
-        msg = f"Login ID: {session['id']}"
-        return render_template(
-            "main.html",
-            title="逆アキネイター(仮)",
-            message=msg,
-            data=message_data,
-        )
-    else:
+    if not session.get("login", False):
         return redirect(url_for("login.show"))
+
+    if "messages" not in session:
+        session["messages"] = []
+
+    message_data = session["messages"]
+
+    msg = f"Login ID: {session['id']}"
+    return render_template(
+        "main.html",
+        title="逆アキネイター(仮)",
+        message=msg,
+        data=message_data,
+    )
 
 
 @view.route("/", methods=["POST"])
 def post():
-    global message_data
     msg = request.form.get("comment")
     typ = request.form.get("action")[:-2]
 
@@ -44,6 +43,14 @@ def post():
     elif typ == "回答":
         ans = model.judge(msg)
 
+    if "messages" not in session:
+        session["messages"] = []
+
+    message_data = session["messages"]
+
     message_data.append((session["id"], typ + ":" + msg + ":" + ans))
     message_data = message_data[-25:]
+
+    session["messages"] = message_data
+
     return redirect(url_for("main.show"))
