2bad60162045ff617a71a0f606c83baef080ead6
komot <komotaymkty@gmail.com>
2023-08-15 22:26:55 +0900

    [refactor - ranking.py] 細かいリファクタ

--- inv_aki_flask/views/ranking.py
+++ inv_aki_flask/views/ranking.py
@@ -10,6 +10,6 @@ def show():
     session_infos = datastore_client.get_public_sessions()
 
     # 順位とのtuple にして template に渡す
-    session_infos = zip(range(1, len(session_infos) + 1), session_infos)
+    session_infos = [(i+1, s) for i, s in enumerate(session_infos)]
 
     return render_template("ranking.html", session_infos=session_infos)
