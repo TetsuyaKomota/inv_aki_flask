063a23ca4fb6df60257fd41b7e993884440f1600
komot <komotaymkty@gmail.com>
2023-08-30 01:24:27 +0900

    [fix - dialog.html] 審査用

--- inv_aki_flask/views/index.py
+++ inv_aki_flask/views/index.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 view = Blueprint("index", __name__, url_prefix="/")
 
 
 @view.route("/", methods=["GET"])
 def show():
-    yyyymmdd = datetime.now().strftime("%Y%m%d")
-    return render_template("index.html", name=yyyymmdd)
+    return redirect(url_for("main.show"))
